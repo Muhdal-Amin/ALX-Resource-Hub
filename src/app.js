a01032aee61a32d5c01ba7ed6d8051156ac3da73
Muhdal-Amin <adalemo2000@gmail.com>
2024-07-05 22:19:59 +0100

    React and Tailwind configuration and implementation for welcome page

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import YourComponent from './YourComponent';
+import './index.css'; // Import Tailwind CSS
+
+const App = () => {
+  return (
+    <div>
+      <YourComponent />
+    </div>
+  );
+};
+
+export default App;
